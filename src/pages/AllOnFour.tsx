f4166a53690d466ae0efc51a55799df4b3c9c16a
unknown <praneelshah.india@gmail.com>
2025-12-18 21:55:15 +0530

    update

--- src/pages/AllOnFour.tsx
+++ src/pages/AllOnFour.tsx
@@ -1,7 +1,7 @@
 import { Card } from "@/components/ui/card";
 import { Button } from "@/components/ui/button";
 import { Link } from "react-router-dom";
-import procedureDiagram from "@/assets/procedure-diagram.jpg";
+import procedureDiagram from "@/assets/img2.jpg";
 import bannerImage from "@/assets/banner-all-on-4.jpg";
 
 const AllOnFour = () => {
